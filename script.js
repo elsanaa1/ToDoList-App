728a18d2c01bd3ea7d4f348bac6ab27a1fb88230
elsanaa1 <sanaa.jerbouh1@gmail.com>
2024-12-19 11:26:04 +0100

    update : function length_input() instead of variable it gets only first length

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+
+
 let input_el=document.getElementById("input_el");
 let btn_add=document.getElementById("add_btn");
 let ul_el=document.querySelector("ul");
 
-let length_input=input.value.length;
+
 
 // add event listeners 
 btn_add.addEventListener("click",addTaskClick);
 input_el.addEventListener("keypress", addTaskKeypress);
 
-
+// function to get the length of input every time 
+function length_input(){
+	return input_el.value.length;
+}
 
 
 function addTaskClick(){
-    	if (length_input > 0) // if the text input is empty,so don't create list element 
-		addNewTask();
+	alert(length_input())
+    	if (length_input()> 0) // if the text input is empty,so don't create list element 
+		  { 
+			addNewTask();}
 }
 
 
 
 function addTaskKeypress(event) {
-	if (length_input > 0 && event.keyCode ===13) { 
+	if (length_input() > 0 && event.keyCode ===13) { 
         //if you click "enter"/"return"
 		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
 		addNewTask();
@@ -29,16 +36,17 @@ function addTaskKeypress(event) {
 
 
 function addNewTask(){
+	
     // creation of new element "li"
     let li_el = document.createElement("li"); 
-	li_el.appendChild(document.createTextNode(input.value)); //li child  is text value 
+	li_el.appendChild(document.createTextNode(input_el.value)); //li child  is text value 
 	ul_el.appendChild(li_el); //adds li to ul
-	input.value = ""; //Reset text input field to empty 
+	input_el.value = ""; //Reset text input field to empty 
 
 
 
     // add delete button to il element 
-    var btn_del = document.createElement("button");
+    let btn_del = document.createElement("button");
 	btn_del.appendChild(document.createTextNode("X"));
 	li_el.appendChild(btn_del);
     // add listener to delete btn
@@ -47,6 +55,14 @@ function addNewTask(){
     function deleteTask(){
         li.classList.add("delete_btn")
     }
+
+
+
+
+
+
+
+
 
 }
 
